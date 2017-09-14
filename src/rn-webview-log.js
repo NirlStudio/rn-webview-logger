834be814764cb8335940551e649d0d351eb2b25c
Leevi <leevi@nirlstudio.com>
2017-09-15 02:22:46 +1200

    fix invalid payload

--- src/rn-webview-log.js
+++ src/rn-webview-log.js
@@ -3,24 +3,30 @@
   var msgType = 'rn-webview-logger/logging'
   window.rnWebViewLogger = {
     info: function () {
+      var payload = ['[WebView] ']
+      Array.prototype.push.apply(payload, arguments)
       window.postJsonMessage && window.postJsonMessage({
         type: msgType,
         logType: 'info',
-        payload: Array.prototype.push.apply(['[WebView] '], arguments)
+        payload: payload
       })
     },
     warn: function () {
+      var payload = ['[WebView] ']
+      Array.prototype.push.apply(payload, arguments)
       window.postJsonMessage && window.postJsonMessage({
         type: msgType,
         logType: 'warn',
-        payload: Array.prototype.push.apply(['[WebView] '], arguments)
+        payload: payload
       })
     },
     error: function () {
+      var payload = ['[WebView] ']
+      Array.prototype.push.apply(payload, arguments)
       window.postJsonMessage && window.postJsonMessage({
         type: msgType,
         logType: 'error',
-        payload: Array.prototype.push.apply(['[WebView] '], arguments)
+        payload: payload
       })
     }
   }
