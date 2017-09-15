319273da558562ea0a08e598e2a5cdcb8c9c031e
Leevi <leevi@nirlstudio.com>
2017-09-15 16:43:46 +1200

    revise & code refactoring

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-import { registerMiddleware } from 'rn-webview-messaging'
-
-const EventLogging = 'rn-webview-logger/logging'
+'use strict'
+const MessageTypeLog = 'rn-webview-logger/MSG_TYPE_LOG'
 
 var logEnabled = __DEV__
+if (logEnabled) {
+  console.info('RNWebViewLogger: logging is enabled.')
+}
 
-registerMiddleware((webView, next) => (event) => {
-  if (event && event.type === EventLogging) {
+export function enableLogging () {
+  logEnabled = true
+}
+
+export function disableLogging () {
+  logEnabled = false
+}
+
+export var RNWebViewLogger = (webView, next) => (event) => {
+  const message = event && event.data
+  if (message && message.type === MessageTypeLog) {
     if (logEnabled) {
-      console[event.logType].apply(console, event.payload)
+      console[message.level].apply(console, message.payload)
     }
   } else {
     next && next(event)
   }
-})
-
-export function enable () {
-  logEnabled = true
-}
-
-export function disable () {
-  logEnabled = false
 }
--- src/rn-webview-logger.js
+++ src/rn-webview-logger.js
@@ -1,36 +1,43 @@
 'use strict';
 (function () {
-  var msgType = 'rn-webview-logger/logging'
-  window.rnWebViewLogger = {
-    info: function () {
-      var payload = ['[WebView] ']
+  var MessageTypeLog = 'rn-webview-logger/MSG_TYPE_LOG'
+  var logPrefix = '[RN-WebView]'
+  // to generate a log writer.
+  var write = function (level) {
+    var localLog = console && (console[level] || console.log)
+    return function () {
+      // try to forward to local console if it does exist.
+      if (localLog) {
+        localLog.apply(console, arguments)
+      }
+      // try to insert a prefix to indicate the log source.
+      var payload = logPrefix ? [logPrefix] : []
       Array.prototype.push.apply(payload, arguments)
+      // post logging message.
       window.postJsonMessage && window.postJsonMessage({
-        type: msgType,
-        logType: 'info',
-        payload: payload
-      })
-    },
-    warn: function () {
-      var payload = ['[WebView] ']
-      Array.prototype.push.apply(payload, arguments)
-      window.postJsonMessage && window.postJsonMessage({
-        type: msgType,
-        logType: 'warn',
-        payload: payload
-      })
-    },
-    error: function () {
-      var payload = ['[WebView] ']
-      Array.prototype.push.apply(payload, arguments)
-      window.postJsonMessage && window.postJsonMessage({
-        type: msgType,
-        logType: 'error',
+        type: MessageTypeLog,
+        level: level,
         payload: payload
       })
     }
   }
+  // create the logger object.
+  var logger = window.rnWebViewLogger = {
+    log: write('log'),
+    info: write('info'),
+    warn: write('warn'),
+    error: write('error'),
+    prefix: function (prefix) {
+      return typeof prefix === 'undefined' ? logPrefix : (logPrefix = prefix)
+    }
+  }
+  // assemble log writers.
+  logger.log.info = logger.info
+  logger.log.warn = logger.warn
+  logger.log.error = logger.error
+  logger.log.prefix = logger.prefix
+  // provide a shortcut by default
   if (typeof window.log === 'undefined') {
-    window.log = window.rnWebViewLogger
+    window.log = logger.log
   }
 })()
